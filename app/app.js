50a1932fe541ac4e2b57d1e2de9cf762b9114377
wen <yui898931@gmail>
2021-09-24 22:18:57 +0800

    前後端連結

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Credentials', false)
+    next()
+})
+
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection // 給一個變數 在連DB @的時候用的
+db.
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ html/src/components/Test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-for="(item,key,index) in data" :key="index">
+    {{key}}
+    <div v-for="(it,inde) in item" :key="inde">
+      {{it}}
+    </div>
+    ------------------------
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      data: {}
+    }
+  },
+  methods: {
+    getData(){
+      console.log()
+      this.axios.get(process.env.VUE_APP_Server_URL+"getJson").then(response=>{
+        this.data = response.data
+      })
+    }
+  },
+  created() {
+    this.getData()
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
